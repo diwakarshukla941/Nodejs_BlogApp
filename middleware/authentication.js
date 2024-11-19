498df5a08b6eb9948938a00091a299415e0e702c
diwakarshukla941 <shukladiwakar941@gmail.com>
2024-11-19 21:27:35 +0530

    updated code

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,28 +1,21 @@
 const { validateToken } = require("../services/authentication");
 
 function checkForAuthenticationCookie(cookieName) {
-    return (req, res, next) => {
-        const tokenCookieValue = req.cookies[cookieName];
+  return (req, res, next) => {
+    const tokenCookieValue = req.cookies[cookieName];
+    if (!tokenCookieValue) {
+      return next();
+    }
 
-        if (!tokenCookieValue) {
-            return next();
-        }
+    try {
+      const userPayload = validateToken(tokenCookieValue);
+      req.user = userPayload;
+    } catch (error) {}
 
-        try {
-            // Attempt to validate the token and set req.user
-            const userPayload = validateToken(tokenCookieValue);
-            req.user = userPayload;
-        } catch (error) {
-            // If token validation fails, log the error and clear req.user
-            console.error("Invalid token:", error.message);
-           
-        }
-
-        // Proceed to the next middleware or route
-       return next();
-    };
+    return next();
+  };
 }
 
 module.exports = {
-    checkForAuthenticationCookie,
+  checkForAuthenticationCookie,
 };
